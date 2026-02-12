b97c14d6acaa95f89695af641034216922ce91cb
Ayler Sabbagh <ayler.sabbagh@gmail.com>
2026-02-12 18:06:36 +0100

    Add CourseService with CreateCourseAsync

---
+++ CoursesManager.Application/Services/CourseService.cs
@@ -0,0 +1,13 @@
+﻿using CoursesManager.Application.Dtos.Courses;
+
+namespace CoursesManager.Application.Services;
+
+//hanterar logik som är kopplad till kurser
+public class CourseService
+{
+    public Task<bool> CreateCourseAsync(CreateCourseDto dto)
+    {
+        return Task.FromResult(true);
+    }
+}
+
