ddf1a2c43639dc229a41a8ccb173a0bd6e0dfc49
Ayler Sabbagh <ayler.sabbagh@gmail.com>
2026-02-12 18:47:14 +0100

    Added ApplicationDbContext

---
+++ CoursesManager.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CoursesManager.Domain.Entities;
+
+namespace CoursesManager.Infrastructure.Persistence;
+
+//hanterar databaskopplingen
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<CourseEntity> Courses { get; set; } = null!;
+}
+
