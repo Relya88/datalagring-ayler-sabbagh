904f891a1bc9f5de3ac66e9faf32af8430a4faf5
Ayler Sabbagh <ayler.sabbagh@gmail.com>
2026-02-13 14:25:01 +0100

    Added CourseRepository

---
+++ CoursesManager.Infrastructure/Persistence/Repositories/CourseRepository.cs
@@ -0,0 +1,13 @@
+﻿using CoursesManager.Domain.Entities;
+
+namespace CoursesManager.Infrastructure.Persistence.Repositories;
+
+//repo för kurspecifik logik
+public class CourseRepository : BaseRepository<CourseEntity>
+{
+    public CourseRepository(ApplicationDbContext context)
+        : base(context)
+    {
+    }
+
+}
