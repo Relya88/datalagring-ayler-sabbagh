97d3715dcb2ff5e43d9aa9139ff349f417de63bb
Ayler Sabbagh <ayler.sabbagh@gmail.com>
2026-02-13 14:05:01 +0100

    Added BaseRepository

---
+++ CoursesManager.Infrastructure/Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoursesManager.Infrastructure.Persistence.Repositories;
+
+//grundrepositoryn med gemensamma metoder
+public class BaseRepository<TEntity> where TEntity : class
+{
+    protected readonly ApplicationDbContext _context;
+    protected readonly DbSet<TEntity> _dbSet;
+
+    public BaseRepository(ApplicationDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<TEntity>();
+    }
+
+    public async Task<TEntity> AddAsync(TEntity entity)
+    {
+        await _dbSet.AddAsync(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+}
